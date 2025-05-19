75bc2dbb84633bdf3cb48a1370305963a9df02cc
gauri <business@itechnolabs.biz>
2025-05-19 14:05:04 +0530

    fix redirection issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/',
-        destination: '/auth/signup',
+        destination: '/signup',
         permanent: true,
       },
     ]
